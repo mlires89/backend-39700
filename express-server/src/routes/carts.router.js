74a60f59a6e72e4de3ec50c0cf421550bd08fda9
Matias Lires <matilires@gmail.com>
2023-02-28 19:13:09 -0300

    preparando primer entrega parcial

--- express-server/src/routes/carts.router.js
+++ express-server/src/routes/carts.router.js
@@ -1,10 +1,32 @@
 import {Router,json} from "express";
-
+import CartManager from "../cartManager.js";
 const cartsRouter = Router();
 cartsRouter.use(json());
 
-cartsRouter.get("/",(req,res)=>{
-    res.send("carritos")
+const cartManager = new CartManager("../carts.json");
+
+cartsRouter.post("/", async (req,res)=>{
+    await cartManager.addCart();
+    res.send({status:"Carrito agregado"})
+})
+
+
+
+cartsRouter.get("/:cid",async (req,res)=>{
+    const cid = Number(req.params.cid);
+    const cart = await cartManager.getCartById(cid);
+    if (!cart){
+        return res.status(400).res.send({error:"No se encontró el carrito"})
+    }
+    res.send({carrito: cart})
+})
+
+
+cartsRouter.post("/:cid/product/:pid",async (req,res)=>{
+    const cartID = Number(req.params.cid);
+    const prodID = Number(req.params.pid);
+    await cartManager.addProdToCart(cartID,prodID);
+    res.send({status:"producto agregado"})
 })
 
 export default cartsRouter;
